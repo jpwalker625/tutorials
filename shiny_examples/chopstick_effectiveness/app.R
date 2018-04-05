fdac796380b4c0ce399b820a2bd21f4ee9ed6022
jpwalker <jpwalker625@gmail.com>
2018-04-04 22:19:33 -0700

    Updating chopstick shiny app

--- shiny_examples/chopstick_effectiveness/app.R
+++ shiny_examples/chopstick_effectiveness/app.R
@@ -9,7 +9,7 @@
 
 library(shiny)
 library(tidyverse)
-
+library(shinythemes)
 #read in dataset
 chopsticks <- read_csv("chopstick-effectiveness.csv")
 
@@ -21,6 +21,8 @@ chopsticks$Chopstick_Length <- factor(chopsticks$Chopstick_Length)
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
+  #for development purposes only 
+  themeSelector(),
    
    # Application title
    titlePanel("Analysis of Chopstick Effectiveness"),
@@ -28,20 +30,27 @@ ui <- fluidPage(
    # Sidebar with a slider input for number of bins 
    sidebarLayout(
       sidebarPanel(
+        wellPanel(
          radioButtons(inputId = "chopstick_length",
                     label = "Select Chopstick Length",
                     choices = levels(chopsticks$Chopstick_Length)),
          numericInput(inputId = "bin_number", label = "Number of Bins",
                       value = 30, min = 1, 
-                      max = 50, step = 1),
+                      max = 50, step = 1)
+        ),
+        wellPanel(
          textInput(inputId = "plot_title", label = "Plot Title", 
                    placeholder = "Enter text to be used as plot title")
+        ),
+        wellPanel(
+         actionButton(inputId = "update_message", label = "Display Message")
+        )
       ),
       
       # Show a plot of the generated distribution
       mainPanel(
          plotOutput("distPlot"),
-         textOutput("plot_info")
+         h6(textOutput("plot_info"))
       )
    )
 )
@@ -60,9 +69,15 @@ server <- function(input, output) {
         labs(title = isolate({tools::toTitleCase(input$plot_title)}))
       })
    
-   output$plot_info <- renderText({
+   update_message <- eventReactive(input$update_message, {
+     req(input$plot_title)
      paste("Notice that the plot title:", input$plot_title, 
-         "doesn't update until one of the other inputs is updated. That's because it is 'isolated'.")
+          "doesn't update until one of the other inputs is updated. 
+          That's because it is 'isolated'. On the other hand, this message is dependent
+          on the `display message` button to update/display and uses an `eventReactive` function.")
+     })
+   
+   output$plot_info <- renderText({update_message()
    })
 }
 
